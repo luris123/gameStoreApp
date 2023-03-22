a539fb09b57d2ad5e8ea816e054da4274fbbd397
Junnu Kyrö <junnukyro@outlook.com>
2023-03-22 21:28:43 +0200

    firebase functions getAllGenres

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,25 @@
 const functions = require("firebase-functions");
+const axios = require("axios");
+const cors = require("cors")({ origin: true });
 
 // // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
- exports.helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
- });
+// // https://firebase.google.com/docs/functions/get-starte
+
+exports.getAllGenres = functions.https.onRequest((request, response) => {
+  cors(request, response, async () => {
+    try {
+      const res = await axios.get(
+        `https://api.rawg.io/api/genres?key=ff1c5d0eb69e428696fd5b80b5cf49df`
+      );
+      response.send({
+        "status": "success",
+        "data": res.data.results
+      });
+    } catch (error) {
+      response.send({
+        "status": error,
+        "data": error
+      });
+    }
+  });
+});
