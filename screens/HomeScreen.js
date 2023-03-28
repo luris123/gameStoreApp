84373464d76cfc28b12434f084cface73d91d94d
luris123 <anssi.rauhala1@gmail.com>
2023-03-28 21:49:16 +0300

    haku toimii

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -17,7 +17,7 @@ const HomeScreen = () => {
  useEffect(() => {
     getAllGenres()
       .then(result => {
-        console.log(result.data);
+        //console.log(result.data);
       })
       .catch(error => {
         console.log(error);
