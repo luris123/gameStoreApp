ea1d59105d7ad1a25fcb45e366df10dd7444b5a2
Junnu Kyrö <junnukyro@outlook.com>
2023-04-18 18:55:45 +0300

    all genres are shown now right

--- screens/GamesByGenreScreen.js
+++ screens/GamesByGenreScreen.js
@@ -9,7 +9,11 @@ const GamesByGenreScreen = ({ route }) => {
     const navigation = useNavigation()
 
     const { genre } = route.params
-    const genreToLowerCase = genre.toLowerCase()
+    let genreToLowerCase = genre.toLowerCase()
+
+    if(genreToLowerCase.includes(" ")){
+        genreToLowerCase = genreToLowerCase.replace(" ", "-")
+    }
 
     const [pageNumber, setPageNumber] = useState(1);
     const [games, setGames] = useState([]);
