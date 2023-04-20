b00603304b0d4d6ffca222f9dfac3224b0362c7a
ArunJ0 <t0nian05@students.oamk.fi>
2023-04-20 16:35:32 +0300

    game page muokattu

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -31,6 +31,12 @@ const GameScreen = ({ navigation, route }) => {
     setShowFullDescription(!showFullDescription);
   };
 
+  const [showFullInfo, setShowFullInfo] = useState(false);
+
+  const toggleInfo = () => {
+    setShowFullInfo(!showFullInfo);
+  };
+
   const [game, setGame] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const randomPrice = Math.floor(Math.random() * 20 + 40)
@@ -128,109 +134,77 @@ const GameScreen = ({ navigation, route }) => {
             )}
           </View>
         </TouchableOpacity>
-        <View style={styles.infoContainer}>
-          <View style={styles.info}>
-            <Text
-              style={
-                theme === "light" ? styles.infoLabelLight : styles.infoLabelDark
-              }
-            >
-              Rating:
+        <TouchableOpacity onPress={toggleInfo}>
+          <View style={theme === "light" ? styles.infoContainerLight : styles.infoContainerDark}>
+            <Text style={theme === "light" ? styles.InfoHeaderLight : styles.InfoHeaderDark} numberOfLines={showFullInfo ? undefined : 2}>
+              Info
             </Text>
-            <Text
-              style={
-                theme === "light" ? styles.infoValueLight : styles.infoValueDark
-              }
-            >
-              {game.rating}
-            </Text>
+            {showFullInfo && (
+            <>
+              <View style={styles.info}>
+                <Text style={theme === "light" ? styles.infoLabelLight : styles.infoLabelDark}>
+                  Price:
+                </Text>
+                <Text style={theme === "light" ? styles.infoValueLight : styles.infoValueDark}>
+                  {game.price}€
+                </Text>
+              </View>
+              <View style={styles.info}>
+                <Text style={theme === "light" ? styles.infoLabelLight : styles.infoLabelDark}>
+                  Rating:
+                </Text>
+                <Text style={theme === "light" ? styles.infoValueLight : styles.infoValueDark}>
+                  {game.rating}
+                </Text>
+              </View>
+              <View style={styles.info}>
+                <Text style={theme === "light" ? styles.infoLabelLight : styles.infoLabelDark}>
+                  Genres:
+                </Text>
+                <Text style={theme === "light" ? styles.infoValueLight : styles.infoValueDark}>
+                  {game.genres}
+                </Text>
+              </View>
+              <View style={styles.info}>
+                <Text style={theme === "light" ? styles.infoLabelLight : styles.infoLabelDark}>
+                  Release Date:
+                </Text>
+                <Text style={theme === "light" ? styles.infoValueLight : styles.infoValueDark}>
+                  {game.releaseDate}
+                </Text>
+              </View>
+              <View style={styles.info}>
+                <Text style={theme === "light" ? styles.infoLabelLight : styles.infoLabelDark}>
+                  Developer:
+                </Text>
+                <Text style={theme === "light" ? styles.infoValueLight : styles.infoValueDark}>
+                  {game.developer}
+                </Text>
+              </View>
+              <View style={styles.info}>
+                <Text style={theme === "light" ? styles.infoLabelLight : styles.infoLabelDark}>
+                  Publisher:
+                </Text>
+                <Text style={theme === "light" ? styles.infoValueLight : styles.infoValueDark}>
+                  {game.publisher}
+                </Text>
+              </View>
+              <View style={styles.info}>
+                <Text style={theme === "light" ? styles.infoLabelLight : styles.infoLabelDark}>
+                  Platforms:
+                </Text>
+                <Text style={theme === "light" ? styles.infoValueLight : styles.infoValueDark}>
+                  {game.platforms}
+                </Text>
+              </View>
+            </>
+            )}
           </View>
-          <View style={styles.info}>
-            <Text
-              style={
-                theme === "light" ? styles.infoLabelLight : styles.infoLabelDark
-              }
-            >
-              Developer:
-            </Text>
-            <Text
-              style={
-                theme === "light" ? styles.infoValueLight : styles.infoValueDark
-              }
-            >
-              {game.developer}
-            </Text>
-          </View>
-          <View style={styles.info}>
-            <Text
-              style={
-                theme === "light" ? styles.infoLabelLight : styles.infoLabelDark
-              }
-            >
-              Publisher:
-            </Text>
-            <Text
-              style={
-                theme === "light" ? styles.infoValueLight : styles.infoValueDark
-              }
-            >
-              {game.publisher}
-            </Text>
-          </View>
-          <View style={styles.info}>
-            <Text
-              style={
-                theme === "light" ? styles.infoLabelLight : styles.infoLabelDark
-              }
-            >
-              Release Date:
-            </Text>
-            <Text
-              style={
-                theme === "light" ? styles.infoValueLight : styles.infoValueDark
-              }
-            >
-              {game.releaseDate}
-            </Text>
-          </View>
-          <View style={styles.info}>
-            <Text
-              style={
-                theme === "light" ? styles.infoLabelLight : styles.infoLabelDark
-              }
-            >
-              Platforms:
-            </Text>
-            <Text
-              style={
-                theme === "light" ? styles.infoValueLight : styles.infoValueDark
-              }
-            >
-              {game.platforms}
-            </Text>
-          </View>
-          <View style={styles.info}>
-            <Text
-              style={
-                theme === "light" ? styles.infoLabelLight : styles.infoLabelDark
-              }
-            >
-              Genres:
-            </Text>
-            <Text
-              style={
-                theme === "light" ? styles.infoValueLight : styles.infoValueDark
-              }
-            >
-              {game.genres}
-            </Text>
-          </View>
-
-          <Button
+        </TouchableOpacity>
+        <Button
             title="Add to shopping cart"
             onPress={handleAddGameToCart}
-          ></Button>
-        </View>
+        ></Button>
       </ScrollView>
     </View>
   );
@@ -244,10 +218,19 @@ const styles = StyleSheet.create({
     height: 200,
     resizeMode: "cover",
   },
-  infoContainer: {
-    marginVertical: 10,
-    alignSelf: "stretch",
-    margin: 20,
+  infoContainerLight: {
+    borderRadius: 5,
+    padding: 5,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    marginBottom: 50,
+  },
+  infoContainerDark: {
+    borderRadius: 5,
+    padding: 5,
+    borderBottomColor: "lightgrey",
+    borderBottomWidth: 1,
+    marginBottom: 50,
   },
   info: {
     flexDirection: "row",
@@ -267,15 +250,29 @@ const styles = StyleSheet.create({
   },
   infoValueLight: {
     fontSize: 16,
-    marginLeft: 8,
-    padding: 5,
+    marginLeft: 10,
+    //padding: 5,
     color: "black",
   },
   infoValueDark: {
     fontSize: 16,
-    marginLeft: 8,
-    padding: 5,
+    marginLeft: 10,
+    //padding: 5,
     color: "lightgrey",
+  },
+  InfoHeaderLight: {
+    fontSize: 20,
+    marginBottom: 15,
+    marginLeft: 10,
+    color: "black",
+    fontWeight: "bold",
+  },
+  InfoHeaderDark: {
+    fontSize: 20,
+    marginBottom: 15,
+    marginLeft: 10,
+    color: "lightgrey",
+    fontWeight: "bold",
   },
 
   descriptionContainerLight: {
@@ -283,46 +280,62 @@ const styles = StyleSheet.create({
     padding: 10,
     borderBottomColor: "black",
     borderBottomWidth: 1,
-    marginBottom: 20,
+    marginBottom: 10,
   },
   descriptionContainerDark: {
     borderRadius: 5,
     padding: 10,
     borderBottomColor: "lightgrey",
     borderBottomWidth: 1,
-    marginBottom: 20,
+    marginBottom: 10,
   },
   descriptionLight: {
     fontSize: 20,
+    marginBottom: 10,
     color: "black",
+    fontWeight: "bold",
   },
   descriptionDark: {
     fontSize: 20,
+    marginBottom: 10,
     color: "lightgrey",
+    fontWeight: "bold",
   },
   descriptionMoreLight: {
     fontSize: 16,
-    marginTop: 10,
     color: "black",
   },
   descriptionMoreDark: {
     fontSize: 16,
-    marginTop: 10,
     color: "lightgrey",
   },
 
-  textMainLightHeader: {
-    fontSize: 24,
+  price: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 8,
+    backgroundColor: "lightblue"
+  },
+  priceLabel: {
+    alignSelf: "center",
+    marginLeft: 15,
+    fontSize: 20,
     fontWeight: "bold",
-    marginTop: 30,
+    color: "black",
+  },
+  textMainLightHeader: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginTop: 5,
     marginBottom: 5,
     color: "black",
     alignSelf: "center",
   },
   textMainDarkHeader: {
-    fontSize: 24,
+    fontSize: 30,
     fontWeight: "bold",
-    marginTop: 30,
+    marginTop: 5,
     marginBottom: 5,
     color: "lightgrey",
     alignSelf: "center",
